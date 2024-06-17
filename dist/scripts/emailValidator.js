9e6c78719166b56648efc213982116ed1694ba4a
Fabio Ura <fabioura10@gmail.com>
2024-06-16 21:30:36 -0300

    feat: implementando validação de email

---
+++ dist/scripts/emailValidator.js
@@ -0,0 +1,19 @@
+"use strict";
+var _a;
+function validarEmail(email) {
+    const emailRegex = /^[^\s@]{2,}@[^\s@]{2,}\.[^\s@]{2,}$/;
+    return emailRegex.test(email);
+}
+(_a = document.getElementById('subscribe-button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    console.log("clicou");
+    const emailInput = document.getElementById('email-input').value;
+    const messageElement = document.getElementById('email-msg');
+    if (validarEmail(emailInput)) {
+        messageElement.textContent = 'E-mail válido!';
+        messageElement.style.color = 'green';
+    }
+    else {
+        messageElement.textContent = 'E-mail inválido!';
+        messageElement.style.color = 'red';
+    }
+});
